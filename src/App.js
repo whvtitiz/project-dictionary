03beb3d5a2d32e0d91cfa63de21a4f4b3446085f
whvtitiz <whitttwiles@gmail.com>
2022-10-09 12:53:23 -0700

    testing bootstrap

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <header className="App-header">
+        <a href="#" className="btn btn-primary">
+          Button
+        </a>
+      </header>
+      The Beginning of my Dictionary App!
     </div>
   );
 }
