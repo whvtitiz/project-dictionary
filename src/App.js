6ac3fc0058411dc24b9559ad492721de0a987f87
whvtitiz <whitttwiles@gmail.com>
2022-10-11 17:37:49 -0700

    Added the Search Engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a href="#" className="btn btn-primary">
-          Button
-        </a>
-      </header>
-      The Beginning of my Dictionary App!
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Whitney Wiles</small>
+        </footer>
+      </div>
     </div>
   );
 }
