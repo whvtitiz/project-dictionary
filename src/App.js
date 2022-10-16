c8ba0cf877e4654aabfefda9ecf8e9b2a03a52d5
whvtitiz <whitttwiles@gmail.com>
2022-10-16 12:24:59 -0700

    Added some CSS

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Dictionary</header>
+        <header className="App-header">❋Dictionary❋</header>
         <main>
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by Whitney Wiles</small>
+          <small>
+            Coded by Whitney Wiles, Open Sourced on{" "}
+            <a href="https://github.com/whvtitiz/project-dictionary">GitHub</a>
+          </small>
         </footer>
       </div>
     </div>
