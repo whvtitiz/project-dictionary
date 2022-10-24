6dafd3d25796f6b4bc26aa95526f4cc959820485
whvtitiz <whitttwiles@gmail.com>
2022-10-24 14:20:35 -0700

    test

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,31 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
-import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
-  let [photos, setPhotos] = useState(null);
 
-  function handleDictionaryResponse(response) {
+  function handleResponse(response) {
     setResults(response.data[0]);
-  }
-
-  function handlePexelsResponse(response) {
-    setPhotos(response.data.photos);
   }
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleDictionaryResponse);
-
-    let pexelsApiKey =
-      "563492ad6f91700001000001af90a2b483884387a55e778bf68f6bdb";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
-    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
-    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleSubmit(event) {
@@ -58,7 +46,6 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
-        <Photos photos={photos} />
       </div>
     );
   } else {
--- src/Photos.js
+++ src/Photos.js
@@ -11,11 +11,7 @@ export default function Photos(props) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img
-                    src={photo.src.landscape}
-                    className="img-fluid"
-                    alt="#"
-                  />
+                  <img src={photo.src.landscape} className="img-fluid" />
                 </a>
               </div>
             );
