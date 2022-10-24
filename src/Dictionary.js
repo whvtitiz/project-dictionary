e1bd5fc5c54edb561ac8f240deadcc0edd9cdcdf
whvtitiz <whitttwiles@gmail.com>
2022-10-24 11:45:55 -0700

    styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -45,7 +45,7 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section>
-          <h1>What word would you want to look up?</h1>
+          <h1>What word do you want to look up?</h1>
           <form onSubmit={handleSubmit}>
             <input
               type="search"
